bbc49b2d651d524c5c9fae96425e2bc438c19944
Admin-it <Admin-it@DESKTOP-1QL8QRP>
2017-09-01 17:36:20 +0530

    LATEST COMMIT ON 5:36PM

--- MavenProjectTest/src/test/java/Com/CrossoverTechTrial/Reusables/Configurations/Configurator.java
+++ MavenProjectTest/src/test/java/Com/CrossoverTechTrial/Reusables/Configurations/Configurator.java
@@ -25,7 +25,7 @@ public static WebDriver driver;
     
    // We Can choose the below browsers as optional firefox/chrome/ie in setup method.
     
-    public void setup(@Optional("firefox")String browser) throws Exception{
+    public void setup(@Optional("chrome")String browser) throws Exception{
         if(browser.equalsIgnoreCase("firefox")){
         	
         	System.setProperty("webdriver.gecko.driver","src\\test\\java\\Com\\CrossoverTechTrial\\Reusables\\Supporters\\Utilities\\geckodriver.exe");
@@ -63,11 +63,11 @@ public static WebDriver driver;
 		driver.get(property.getProperty("url"));
 		Reporter.log("Url Entered");
 		
-		Thread.sleep(80000);
+		Thread.sleep(4000);
 		CrossoverTechTrialLoginPage.EnterEmailid(driver);
 		Reporter.log("Email entered");
 		
-		Thread.sleep(8000);
+		Thread.sleep(4000);
 		CrossoverTechTrialLoginPage.EnterPassword(driver);
 		Reporter.log("Password entered");
 		
@@ -78,11 +78,11 @@ public static WebDriver driver;
     }
    
     
-/*@AfterTest
+@AfterTest
     public void destroy() {
 		driver.quit();
 
-	}*/
+	}
     
 	
 }
